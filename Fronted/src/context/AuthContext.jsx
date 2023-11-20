682ce4891605d1da14e8dfd5e4b13377876eb2cf
Luis David Rambao <lrambao5432@gmail.com>
2023-11-20 12:13:42 -0500

    :zap: improve AuthContext

--- Fronted/src/context/AuthContext.jsx
+++ Fronted/src/context/AuthContext.jsx
@@ -1,3 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export const AuthContext = createContext("");
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token");
+        if (storedToken) {
+            setUser({ token: storedToken });
+        }
+    }, []);
+
+    const login = (userData, token, navigate) => {
+        localStorage.setItem("token", token);
+        setUser({ ...userData, token });
+        navigate("/home");
+    };
+
+    const logout = (navigate) => {
+        localStorage.removeItem("token");
+        setUser(null);
+        navigate("/login");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
