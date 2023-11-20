ca8dd30d383543bcb9481eef9e4fdbd01044f558
Luis David Rambao <lrambao5432@gmail.com>
2023-11-20 12:19:06 -0500

    :zap: add routes of the project

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import Login from './pages/login/login'
 import Register from './pages/register/register'
-
+import Layout from './components/Layout'
+import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
+
   return (
-    <>
-      <Register />
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Layout />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
