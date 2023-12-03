4226c7a57f00d5c0ec50f5ae64f7c9313e484477
Luis David Rambao <lrambao5432@gmail.com>
2023-12-03 14:13:38 -0500

    :recycle: new things

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -4,25 +4,28 @@ import Register from './pages/register/register'
 import Layout from './components/Layout'
 import { AuthProvider } from './context/AuthContext'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-
-
+import { WishlistProvider } from './context/WishlistContext'
+import Wishlist from './pages/wishlist/wishlist'
+import Cart from './pages/cart/cart'
 
 
 function App() {
 
 
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home/*" element={<Layout />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <WishlistProvider>
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </WishlistProvider>
+      </AuthProvider>
+    </Router>
   )
 }
 
