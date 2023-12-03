8d3cb49fb0dc6e14ab53c4a7e600c8bdcffc88fe
Luis David Rambao <lrambao5432@gmail.com>
2023-12-03 14:09:57 -0500

    :recycle: storage to backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/routesProducts');
+const wishlistRoutes = require('./routes/whishlistRoutes');
+const bodyParser = require('body-parser');
+const cartRoutes = require('./routes/cartRoutes');
 require('dotenv').config({ path: './.env' });
 
 const app = express();
@@ -10,8 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use('/api', authRoutes);
 app.use('/api/productos', productRoutes);
+app.use('/api', wishlistRoutes);
+app.use('/api', cartRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
