0b7539ecce63f05df63ba8e2dea2f4b0085eedf8
Luis David Rambao <lrambao5432@gmail.com>
2023-12-03 14:12:13 -0500

    :sparkles: add utils

---
+++ Fronted/src/utils/networking.js
@@ -0,0 +1,11 @@
+const SERVICE_PATH = {
+    login: "http://localhost:3001/api/login",
+    register: "http://localhost:3001/api/register",
+    getProducts: "http://localhost:3001/api/productos/",
+    addWishList: "http://localhost:3001/api/whish",
+    removeWishList: "http://localhost:3001/api/wishlist/",
+}
+
+
+
+export default SERVICE_PATH
